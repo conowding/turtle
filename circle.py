741cf0dfb49a69a6fc1d5bbca2e644851bcfad23
salmon <jchuroo@naver.com>
2022-07-24 09:49:27 +0900

    Turtle Library

---
+++ circle.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+n = 60
+shape('turtle')
+speed('fastest')
+for i in range(n):
+    circle(120)
+    right(360 / n)
+YR7T
+end_fill()
+done()
---
+++ vortex.py
@@ -0,0 +1,9 @@
+from turtle import *
+
+shape('turtle')
+speed('fastest')
+for i in range(300):
+    forward(i)
+    right(91)
+end_fill()
+done()
